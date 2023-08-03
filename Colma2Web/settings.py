504c8f62ce900b6e00eb1b2128ac9fff14a47f26
Gonzalo Luque <gonzalo.luque@dacadoo.com>
2023-08-03 23:16:43 +0200

    media

--- Colma2Web/settings.py
+++ Colma2Web/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9z&uti6ckqo5e4%nkp4aa035lrn+#0%ue*pasmcs8&ik#lamr+'
 
 ALLOWED_HOSTS = ['colma2.azurewebsites.net', 'localhost']
-os.environ["DEBUG"] = True
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("acacaca", True)
 
 
 # Application definition
